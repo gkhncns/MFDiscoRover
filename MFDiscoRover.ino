1d22e5361c4f244eb1cf63abd0575d45bcb0319c
Gokhan <Gokhan@Gokhans-MacBook-Pro.local>
2016-05-10 22:47:40 +0300

    v1.0

---
+++ MFDiscoRover.ino
@@ -0,0 +1,181 @@
+/*
+ * MultiFunctional Discovery Robot
+ *
+ * 12/25/2015 - Gokhan Cansu | Onur Erdogan
+*/
+
+// FrontDC Implementation
+//int enableFrontDC = 11;
+//int rightInput = 10;
+//int leftInput = 9;
+
+// BackDC Implementation
+int enableBackDC = 3;
+int forwardInput = 5;
+int backInput = 6;
+
+// Including necessary libraries
+#include <Bridge.h>
+#include <YunServer.h>
+#include <YunClient.h>
+#include <Process.h>
+#include <FileIO.h>
+#include <Servo.h>
+#include "DHT.h"
+
+// Defining pins and sensor types
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+// Initialize
+DHT dht(DHTPIN, DHTTYPE);
+
+YunServer server;
+Process process;
+
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;
+
+void setup() {
+  // Set motors output pins.
+  pinMode(backInput, OUTPUT);
+  pinMode(forwardInput, OUTPUT);
+  pinMode(enableBackDC, OUTPUT);
+
+  // Start serial communication.
+  Serial.begin(9600);
+
+  // Bridge startup.
+  Bridge.begin();
+
+  // File system begin.
+  FileSystem.begin();
+
+  // Dht begin.
+  dht.begin();
+
+  myservo.attach(8);
+
+  // Listen for incoming connection only from localhost.
+  // (no one from the external network could connect)
+  server.listenOnLocalhost();
+  server.begin();
+}
+
+void loop() {
+  // Set front dc turning speed.
+//  int speedForFrontDC = 255;
+  int speedForBackDC = 150;
+
+  // Get clients coming from server
+  YunClient client = server.accept();
+
+  // Writing data to file from sensors.
+  printSensorData();
+
+  // There is a new client?
+  if (client) {
+    String command = client.readStringUntil('/');
+
+    // is "onOff" command?
+    if (command == "onOff") {
+      int stat = client.parseInt();
+      if (stat == 1) {
+        process.runShellCommand("/etc/init.d/mjpg-streamer start");
+      }
+      else {
+        process.runShellCommand("/etc/init.d/mjpg-streamer stop");
+        delay(1000);
+      }
+      // Close connection and free resources.
+      client.stop();
+    }
+
+    // is "screenShot" command?
+    if (command == "screenShot") {
+      int stat = client.parseInt();
+      if (stat == 1) {
+        process.runShellCommand("/etc/init.d/mjpg-streamer stop");
+        process.runShellCommand("fswebcam screenShot.png");
+        process.runShellCommand("mv /usr/lib/python2.7/bridge/screenShot.png /mnt/sda1/arduino/www/MFDiscoRover/img/");
+        process.runShellCommand("/etc/init.d/mjpg-streamer start");
+      }
+
+      // Close connection and free resources.
+      client.stop();
+    }
+
+    // is "servo" command?
+    if (command == "servo") {
+
+      int pos = client.parseInt();
+      myservo.write(pos);
+
+      client.stop();
+    }
+
+    // is "frontDC" command?
+//    if (command == "frontDC") {
+//      int stat = client.parseInt();
+//      if (stat == 1) {
+//        setMotorFront(speedForFrontDC, true);
+//      }
+//      else if (stat == 0) {
+//        setMotorFront(speedForFrontDC, false);
+//      }
+//      // Close connection and free resources.
+//      client.stop();
+//    }
+//
+    // is "move" command?
+    if (command == "move") {
+      int stat = client.parseInt();
+      if (stat == 1) {
+        setMotorBack(speedForBackDC, true);
+      }
+      else {
+        setMotorBack(speedForBackDC, false);
+      }
+      // Close connection and free resources.
+      client.stop();
+    }
+
+
+    delay(100);
+  }
+}
+
+void printSensorData() {
+  String dataString;
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  dataString += (String)h;
+  dataString += ",";
+  dataString += (String)t;
+
+  File dataFile = FileSystem.open("/mnt/sda1/arduino/www/MFDiscoRover/datalog.txt", FILE_WRITE);
+
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+  }
+}
+
+//void setMotorFront(int speed, boolean reverse)
+//{
+//  analogWrite(enableFrontDC, speed);
+//  digitalWrite(leftInput, ! reverse);
+//  digitalWrite(rightInput, reverse);
+//}
+
+void setMotorBack(int speed, boolean reverse)
+{
+  analogWrite(enableBackDC, speed);
+  digitalWrite(forwardInput, ! reverse);
+  digitalWrite(backInput, reverse);
+}
+
---
+++ www/js/discorover.js
@@ -0,0 +1,109 @@
+play = true;
+// var streamLink = "http://172.20.10.8:8080/?action=stream";
+var streamLink = "http://192.168.1.7:8080/?action=stream";
+var humData = [];
+var tempData = [];
+var chartTemp;
+var chartHum;
+
+$(document).ready(function () {
+    
+    chartTemp = new CanvasJS.Chart("chartTemperature", {
+        title: {
+            text: "Temperature"
+        },
+        data: [{
+            type: "line",
+            dataPoints: tempData
+        }]
+    });
+
+    chartHum = new CanvasJS.Chart("chartHumidity", {
+        title: {
+            text: "Humidity"
+        },
+        data: [{
+            type: "line",
+            dataPoints: humData
+        }]
+    });
+    
+    setInterval (function (){
+    $.get( "datalog.txt", function( data ) {
+    var array = data.split(',');
+    if (array[0]){
+        if (array[1]){
+            console.log(array[0]);
+            console.log(array[1]);
+            updateChart(parseInt(array[0]), parseInt(array[1]))
+        }
+    }})}, 3000);
+
+
+    streamOn();
+    setTimeout(function () { $("#stream").attr("src", streamLink); }, 3000);
+
+    $('#onOff').click(function () {
+        if (play == true) {
+            streamOff();
+            console.log("onOff button was clicked and stream is closed.");
+            play = false;
+        } else {
+            streamOn();
+            setTimeout(function () { $("#stream").attr("src", streamLink); }, 3000);
+            console.log("onOff button was clicked and stream is opened.");
+            play = true;
+        }
+    });
+
+    $('#screenShot').click(function () {
+       console.log("screenShot button was clicked.");
+       screenShot();
+       setTimeout(function () { $("#stream").attr("src", streamLink); }, 6000);
+    });
+    
+    $('#steering').on('change', function(){
+        console.log($(this).val());
+        $.get('/arduino/servo/' + $(this).val(), function () { });
+    });
+    
+    $('#frontDCRight').click(function(){
+        console.log("right clicked");
+        $.get('/arduino/frontDC/1', function () { });
+    });
+    
+    $('#frontDCLeft').click(function(){
+        console.log("left clicked");
+        $.get('/arduino/frontDC/0', function () { });
+    });
+    
+    $('#moveForward').click(function(){
+        console.log("forward clicked");
+        $.get('/arduino/move/1', function () { });
+    });
+    
+    $('#moveBack').click(function(){
+        console.log("back clicked");
+        $.get('/arduino/move/0', function () { });
+    });
+});
+
+function streamOn() {
+    $.get('/arduino/onOff/1', function () { });
+}
+
+function streamOff() {
+    $.get('/arduino/onOff/0', function () { });
+}
+
+function screenShot() {
+    $.get('/arduino/screenShot/1', function () { });
+}
+
+function updateChart(hum, temp) {
+    humData.push({x:humData.length + 1 , y : hum}); 
+    tempData.push({x:tempData.length + 1 , y : temp});
+    
+    chartTemp.render();
+    chartHum.render();
+}
